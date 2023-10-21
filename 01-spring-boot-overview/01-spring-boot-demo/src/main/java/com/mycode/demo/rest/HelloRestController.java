dcb0b43a6d0560198778e09f3c40fedcecf67271
Nihat Çetin <96945187+nihatctnn@users.noreply.github.com>
2023-10-21 14:48:04 +0300

    Add files via upload

---
+++ 01-spring-boot-overview/01-spring-boot-demo/src/main/java/com/mycode/demo/rest/HelloRestController.java
@@ -0,0 +1,16 @@
+package com.mycode.demo.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloRestController {
+
+    // expose "/" that return "Hello World"
+
+    @GetMapping("/")
+    public String sayHello(){
+        return "Hello World";
+    }
+
+}
---
+++ 01-spring-boot-overview/02-dev-tools-demo/src/main/java/com/mycode/demo/rest/HelloRestController.java
@@ -0,0 +1,28 @@
+package com.mycode.demo.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloRestController {
+
+    // expose "/" that return "Hello World"
+
+    @GetMapping("/")
+    public String sayHello(){
+        return "Hello World";
+    }
+
+    // expose a new endpoint for "workout"
+    @GetMapping("/workout")
+    public String getDailyWorkout(){
+        return "Run a hard 10k !";
+    }
+
+    // expose a new endpoint for "fortune"
+    @GetMapping("/fortune")
+    public String getFortune(){
+        return "Today is your lucky day !";
+    }
+
+}
